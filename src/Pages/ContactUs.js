d3011d7920bd425677a08e1419b562204718c496
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-04-06 10:56:21 -0700

    Contact Page Links

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -14,25 +14,25 @@ function ContactUs() {
         <Link to="/"> 🡠 Back</Link>
         <div className="contact2">
           <h2>Call</h2>
-          <a href="/">
+          <a href="tel:+15592683494">
             <img className="icon" src={phone} alt="Phone" />
           </a>
         </div>
         <div className="contact2">
           <h2>Email</h2>
-          <a href="/">
+          <a href="mailto: pacificapizzatower@gmail.com">
             <img className="icon" src={email} alt="Email" />
           </a>
         </div>
         <div className="contact2">
           <h2>Social Media</h2>
-          <a href="/">
+          <a href="https://www.facebook.com/Pacifica-Pizza-151158698289170/">
             <img className="icon" src={fb} alt="FaceBook" />
           </a>
-          <a href="/">
+          <a href="https://www.instagram.com/pacifica_pizza/">
             <img className="icon" src={ig} alt="Instagram" />
           </a>
-          <a href="/">
+          <a href="https://twitter.com/pacpizzatower">
             <img className="icon" src={twitter} alt="Twitter" />
           </a>
         </div>
